c7c373977411f29cf0bebc9eb0d46c11a674c527
sonlh02 <let02hb@gmail.com>
2024-11-26 16:37:59 +0700

    fix bug viewinsdoc

--- src/app/se/(auth)/(menu)/inspect-docs/view/[id]/page.tsx
+++ src/app/se/(auth)/(menu)/inspect-docs/view/[id]/page.tsx
@@ -214,7 +214,7 @@ export default function ViewDoc({ params }: { params: { id: string } }) {
   )
 }
 
-function InfoItem({ label, value }: { label: string; value: string }) {
+function InfoItem({ label, value }: { label: string; value: string | number }) {
   return (
     <div className="space-y-1">
       <p className="text-sm font-medium text-muted-foreground">{label}</p>
